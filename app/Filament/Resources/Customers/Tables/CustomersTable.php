273665b88447f118cd92476880f5c30092015c80
jajla <kamilmichalski204@gmail.com>
2026-01-31 22:22:43 +0100

    powrot do punktu wyjscia

--- app/Filament/Resources/Customers/Tables/CustomersTable.php
+++ app/Filament/Resources/Customers/Tables/CustomersTable.php
@@ -23,72 +23,22 @@ class CustomersTable
         return $table
             ->paginated([25, 50, 100])
             ->defaultPaginationPageOption(100)
-            ->columns([/*
-                Grid::make([
-                    'sm' => 1, // 📱 mały → 1 kolumna
-                    'md' => 3, // 💻 średni → 3 kolumny
-                    'xl' => 6, // 🖥 duży → 6 kolumn = 1 rząd (bo 6 pól)
-                ])->schema([
-                    TextColumn::make('id'),
-                    TextColumn::make('role'),
-                    TextColumn::make('name'),
-                    TextColumn::make('last_name'),
-                    TextColumn::make('email'),
-                    TextColumn::make('phone_number'),
-                ]),
-                Grid::make([
-                    'sm' => 1,
-                    'md' => 3,
-                    'lg' => 6, // 3 Stacki w jednym rzędzie
-                ])->schema([
-                    Stack::make([
-                        TextColumn::make('id'),
-                        TextColumn::make('role'),
-                    ]),
-                    Stack::make([
-                        TextColumn::make('name'),
-                        TextColumn::make('last_name'),
-                    ]),
-                    Stack::make([
-                        TextColumn::make('email'),
-                        TextColumn::make('phone_number'),
-                    ]),
-                ]),*/
-                // MOBILE / SMALL: 1 kolumna
-                Stack::make([
-                    TextColumn::make('id'),
-                    TextColumn::make('role'),
-                    TextColumn::make('name'),
-                    TextColumn::make('last_name'),
-                    TextColumn::make('email'),
-                    TextColumn::make('phone_number'),
-                ])->hiddenFrom('md'),
-
-                // MEDIUM: 3 kolumny (Split zawierający 3 Stacki)
+            ->columns([
                 Split::make([
                     Stack::make([
-                        TextColumn::make('id'),
-                        TextColumn::make('role'),
+                        TextColumn::make('id')->alignCenter(),
+                        TextColumn::make('role') ->formatStateUsing(fn($state) => $state->getLabel())->icon('heroicon-m-shield-check')->alignCenter(),
                     ]),
                     Stack::make([
-                        TextColumn::make('name'),
-                        TextColumn::make('last_name'),
+                        TextColumn::make('name')->alignCenter()->icon('heroicon-m-user'),
+                        TextColumn::make('last_name')->icon('heroicon-m-user')->alignCenter(),
                     ]),
                     Stack::make([
-                        TextColumn::make('email'),
-                        TextColumn::make('phone_number'),
-                    ]),
-                ])->from('md')->hiddenFrom('xl'),
+                        TextColumn::make('email')->icon('heroicon-m-at-symbol')->alignCenter(),
+                        TextColumn::make('phone_number')->icon('heroicon-m-phone')->alignCenter(),
 
-                // XL: 1 rząd (Split z wszystkimi kolumnami)
-                Split::make([
-                    TextColumn::make('id'),
-                    TextColumn::make('role'),
-                    TextColumn::make('name'),
-                    TextColumn::make('last_name'),
-                    TextColumn::make('email'),
-                    TextColumn::make('phone_number'),
-                ])->from('sm'),
+                    ])
+                ])->from('lg'),
             ])
             ->filters([
                 //
