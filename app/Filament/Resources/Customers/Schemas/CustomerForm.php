814d82e53212300c1ea58d1f7d344148f7cb78b2
jajla <kamilmichalski204@gmail.com>
2026-02-01 16:14:37 +0100

    dodanie 2 sekcji w edycji klienta

--- app/Filament/Resources/Customers/Schemas/CustomerForm.php
+++ app/Filament/Resources/Customers/Schemas/CustomerForm.php
@@ -14,7 +14,43 @@ class CustomerForm
     {
         return $schema
             ->components([
-                Section::make()
+                // ===== SEKCJA 1: 2 pola obok siebie =====
+                Section::make('')
+                    ->columns(2)
+                    ->columnSpan('full')
+                    ->schema([
+                        TextInput::make('name')
+                            ->label(__('filament-panels::auth/pages/register.form.name.label')),
+                        TextInput::make('last_name')
+                            ->label(__('filament-panels::auth/pages/register.form.last_name.label')),
+                    ]),
+
+                // ===== SEKCJA 2: 3 pola obok siebie =====
+                Section::make('')
+                    ->columns(3)
+                    ->columnSpan('full')
+                    ->schema([
+                        TextInput::make('phone_number')
+                            ->label(__('filament-panels::auth/pages/register.form.phone_number.label')),
+                        TextInput::make('email')
+                            ->reactive()
+                            ->disabled(fn($get) => $get('role') === Roles::Guest->value)
+                            ->afterStateHydrated(function ($component, $state, $record) {
+                                if ($record && $record->role === Roles::Guest->value) {
+                                    $component->disabled(true);
+                                }
+                            }),
+                        Select::make('role')
+                            ->label(__('filament-panels::auth/pages/register.form.role.label'))
+                            ->reactive()
+                            ->options(
+                                collect(Roles::cases())
+                                    ->mapWithKeys(fn($role) => [$role->value => $role->getLabel()])
+                                    ->toArray()
+                            ),
+                    ]),
+
+               /* Section::make()
                     ->columns(2)// dwie kolumny w sekcji
                         ->columnSpan(2)
                     ->schema([
@@ -27,7 +63,13 @@ class CustomerForm
                         ),
                         TextInput::make('email'),
                         TextInput::make('phone_number'),
-                    ])
+                    ])*/
+
+
+
+
+                /*orginalny wyglad*/
+
         /*        TextInput::make('name')
                     ->label(__('filament-panels::auth/pages/register.form.name.label')),
                 TextInput::make('last_name')
