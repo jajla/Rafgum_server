e0065c6450f2682df5b92357799c3d58b98a1964
jajla <kamilmichalski204@gmail.com>
2026-02-01 23:03:44 +0100

    favion by na tel dzialalo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AutoWulkanizacja Janek</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="icon" href="/favicon.png?v=2">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="manifest" href="/site.webmanifest" />
 
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
