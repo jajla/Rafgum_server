449e241a8fb8a27747169ba32fe2e5db18f29a4f
Kamil <kamilmichalski204@gmail.com>
2026-05-17 14:27:45 +0200

    dodanie visit service

--- app/Filament/Resources/Visits/Schemas/VisitForm.php
+++ app/Filament/Resources/Visits/Schemas/VisitForm.php
@@ -2,29 +2,53 @@
 
 namespace App\Filament\Resources\Visits\Schemas;
 
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\TimePicker;
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Utilities\Get;
+
+use App\Services\VisitService;
+use App\Enums\Role;
+use App\Enums\Services;
+use App\Models\User;
+use App\Models\Visit;
 
 class VisitForm
 {
     public static function configure(Schema $schema): Schema
     {
+        $dateFormat = 'Y-m-d';
         return $schema
             ->components([
-                TextInput::make('user_id')
+                Select::make('user_id')
+                    ->label(__('trans.form.user_id'))
+                    ->native(false)
+                    ->options(User::query()
+                        ->get()
+                        ->mapWithKeys(fn($user) => [$user->id => $user->last_name]))
+                    ->required(),
+               DatePicker::make('date')
+                    ->minDate(now()->format($dateFormat))
+                    //->native(false)
                     ->required()
-                    ->numeric(),
-                DatePicker::make('date')
-                    ->required(),
-                TimePicker::make('time')
-                    ->required(),
-                TextInput::make('service_type')
-                    ->required(),
-                TextInput::make('status')
+                   //->displayFormat('d F Y')
+                    ->live(),
+                Select::make('time')
+                    ->options(fn(Get $get) => (new VisitService())->getAvailableTimesForDate($get('date')))
+                    ->hidden(fn(Get $get) => !$get('date'))
                     ->required()
-                    ->default('pending'),
+                    ->formatStateUsing(fn($state) => date('H:i', strtotime($state)))
+                    ->native(false),
+
+                Select::make('service_type')
+                    ->label(__('trans.form.service_type'))
+                    ->options(
+                        collect(Services::cases())
+                            ->mapWithKeys(fn($role) => [$role->value => $role->getLabel()])
+                            ->toArray()
+                    )->required(),
             ]);
     }
 }
---
+++ app/Services/VisitService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Visit;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+
+class VisitService
+{
+    public function getAvailableTimesForDate(?string $date): array
+    {
+        if (!$date) {
+            return [];
+        }
+
+        $date = Carbon::parse($date);
+
+        $startPeriod = $date->copy()->hour(9);
+        $endPeriod = $date->copy()->hour(18);
+
+        $times = CarbonPeriod::create(
+            $startPeriod,
+            '30 minutes',
+            $endPeriod
+        );
+
+        $reservations = Visit::whereDate('date', $date)
+            ->pluck('time')
+            ->map(fn($time) => Carbon::parse($time)->format('H:i'))
+            ->toArray();
+
+        $availableReservations = [];
+
+        foreach ($times as $time) {
+            $formattedTime = $time->format('H:i');
+
+            if (!in_array($formattedTime, $reservations)) {
+                $availableReservations[$formattedTime] = $formattedTime;
+            }
+        }
+
+        return $availableReservations;
+    }
+}
