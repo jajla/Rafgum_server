b89acbaab2ddf916ad2b9a820ad8fb43ff8a8243
kamil <kamilmichalski09@gmail.com>
2026-05-18 23:11:53 +0200

    poprawa z role na roles

--- app/Filament/Resources/Visits/Pages/CreateVisit.php
+++ app/Filament/Resources/Visits/Pages/CreateVisit.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\Visits\Pages;
 
 use App\Filament\Resources\Visits\VisitResource;
 use Filament\Resources\Pages\CreateRecord;
+use App\Enums\Roles;
 
 class CreateVisit extends CreateRecord
 {
@@ -12,7 +13,7 @@ class CreateVisit extends CreateRecord
      protected function mutateFormDataBeforeCreate(array $data): array
     {
         // jeśli nie admin -> ustaw aktualnego usera
-        if (auth()->user()?->role !== Role::ADMIN) {
+        if (auth()->user()?->role !== Roles::Admin) {
             $data['user_id'] = auth()->id();
         }
 
