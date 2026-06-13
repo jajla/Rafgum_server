418eedeeecc4afe4051eb6daef666490ac44396d
Kamil <kamilmichalski204@gmail.com>
2026-06-13 16:23:21 +0200

    formatowanie pliku

--- app/Filament/Resources/Visits/Schemas/VisitForm.php
+++ app/Filament/Resources/Visits/Schemas/VisitForm.php
@@ -23,68 +23,61 @@ class VisitForm
         $dateFormat = 'Y-m-d';
 
         return $schema
-            ->components([    
-
-                 
+            ->components([
                 Section::make()
                     ->columnSpan('full')
                     ->columns(2)
                     ->schema([
-            Section::make('')
-            ->schema([
-                Select::make('user_id')
-                    ->label(__('trans.Resources.Visits.user'))
-                    ->native(false)
-                    ->options(User::query()
-                        ->get()
-                        ->mapWithKeys(fn($user) => [$user->id => $user->last_name]))
-                    ->required()
-                    ->visible(fn () => auth()->user()?->role === Roles::Admin)
-                    ->required(fn () => auth()->user()?->role === Roles::Admin),
-               DatePicker::make('date')
-               ->label(__('trans.Resources.Visits.date'))
-                    ->minDate(now()->format($dateFormat))
-                    //->native(false)
-                    ->required()
-                   //->displayFormat('d F Y')
-                    ->live(),
+                        Section::make('')
+                            ->schema([
+                                Select::make('user_id')
+                                    ->label(__('trans.Resources.Visits.user'))
+                                    ->native(false)
+                                    ->options(User::query()
+                                        ->get()
+                                        ->mapWithKeys(fn($user) => [$user->id => $user->last_name]))
+                                    ->required()
+                                    ->visible(fn() => auth()->user()?->role === Roles::Admin)
+                                    ->required(fn() => auth()->user()?->role === Roles::Admin),
+                                DatePicker::make('date')
+                                    ->label(__('trans.Resources.Visits.date'))
+                                    ->minDate(now()->format($dateFormat))
+                                    //->native(false)
+                                    ->required()
+                                    //->displayFormat('d F Y')
+                                    ->live(),
+                            ]),
 
-            ]),
+                        Section::make([''])->schema([
+                            // Select::make('time')
+                            //     ->label(__('trans.Resources.Visits.time'))
+                            //     ->options(fn(Get $get) => (new VisitService())->getAvailableTimesForDate($get('date')))
+                            //     ->hidden(fn(Get $get) => !$get('date'))
+                            //     ->required()
+                            //     ->formatStateUsing(fn($state) => date('H:i', strtotime($state)))
+                            //     ->native(false),
 
-          
-                Section::make([''])->schema([
+                            Select::make('time')
+                                ->label(__('trans.Resources.Visits.time'))
+                                ->options(
+                                    fn(Get $get, $record) => (new VisitService())->getAvailableTimesForDate(
+                                        $get('date'),
+                                        $record?->id
+                                    )
+                                )
+                                ->required()
+                                ->formatStateUsing(fn($state) => date('H:i', strtotime($state)))
+                                ->native(false),
 
-              
-         
-                // Select::make('time')
-                //     ->label(__('trans.Resources.Visits.time'))
-                //     ->options(fn(Get $get) => (new VisitService())->getAvailableTimesForDate($get('date')))
-                //     ->hidden(fn(Get $get) => !$get('date'))
-                //     ->required()
-                //     ->formatStateUsing(fn($state) => date('H:i', strtotime($state)))
-                //     ->native(false),
-
-                Select::make('time')
-    ->label(__('trans.Resources.Visits.time'))
-    ->options(fn(Get $get, $record) =>
-        (new VisitService())->getAvailableTimesForDate(
-            $get('date'),
-            $record?->id
-        )
-    )
-    ->required()
-    ->formatStateUsing(fn($state) => date('H:i', strtotime($state)))
-    ->native(false),
-
-                Select::make('service_type')
-                    ->label(__('trans.Resources.Visits.service'))
-                    ->options(
-                        collect(Services::cases())
-                            ->mapWithKeys(fn($role) => [$role->value => $role->getLabel()])
-                            ->toArray()
-                    )->required(),
-                ])
+                            Select::make('service_type')
+                                ->label(__('trans.Resources.Visits.service'))
+                                ->options(
+                                    collect(Services::cases())
+                                        ->mapWithKeys(fn($role) => [$role->value => $role->getLabel()])
+                                        ->toArray()
+                                )->required(),
+                        ])
                     ])
-        ]);
+            ]);
     }
 }
