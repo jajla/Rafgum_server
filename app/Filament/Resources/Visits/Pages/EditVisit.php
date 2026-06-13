12c27094cb68bd7ac8db5075fbad803ec09a4366
Kamil <kamilmichalski204@gmail.com>
2026-06-13 15:12:11 +0200

    dodanie 2 redirect

--- app/Filament/Resources/Visits/Pages/EditVisit.php
+++ app/Filament/Resources/Visits/Pages/EditVisit.php
@@ -16,4 +16,9 @@ class EditVisit extends EditRecord
             DeleteAction::make(),
         ];
     }
+    
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
